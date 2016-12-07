18a6994f479220929de97873e16d0a3eb9144e33
Aaron Dancer <me@aarondancer.com>
2016-12-06 20:16:38 -0600

    Return distance instead of true if coordinate is within threshold

--- haversine.js
+++ haversine.js
@@ -26,9 +26,10 @@ var haversine = (function () {
     var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
             Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2)
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-
+    var distance = R * c
+    
     if (options.threshold) {
-      return options.threshold > (R * c)
+      return options.threshold > distance ? distance : false
     }
 
     return R * c
